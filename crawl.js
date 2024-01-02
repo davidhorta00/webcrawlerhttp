b5b3e2c064b48fedba5309deff177e69ddc11256
davidhorta <davidhorta00@hotmail.com>
2024-01-02 09:51:12 +0000

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,49 +1,74 @@
-const {JSDOM} = require('jsdom') 
+const { JSDOM } = require('jsdom')
 
-function getURLsFromHTML(htmlBody, baseURL){
+async function crawlPage(currentURL) {
+    console.log(`Actively crawling: ${currentURL}`)
+    try {
+        const resp = await fetch(currentURL)
+        if (resp.status > 399) {
+            console.log(`Error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+         const contentType = resp.headers.get("content-type")
+         if(!contentType.includes("text/html")){
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`)
+            return
+         }
+
+        console.log(await resp.text())
+
+
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page ${currentURL}`)
+    }
+}
+
+function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
     const dom = new JSDOM(htmlBody)
     const linkElements = dom.window.document.querySelectorAll("a")
-    for (const linkElement of linkElements){
-        if(linkElement.href.slice(0,1) === "/"){
+
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === "/") {
             // relative url
-            try{
+            try {
                 const urlObj = new URL(`${baseURL}${linkElement.href}`)
                 urls.push(urlObj.href)
-            } catch(err){
+            } catch (err) {
                 console.log(`Error with relative url:${err.message}`)
             }
         }
-        else{
+        else {
             // absolute url
 
-            try{
+            try {
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
-            } catch(err){
+            } catch (err) {
                 console.log(`Error with absolute url:${err.message}`)
             }
-            
+
         }
-     
+
     }
     return urls
 }
 
 
-function normalizeURL (urlString){
+function normalizeURL(urlString) {
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if(hostPath.length > 0 && hostPath.slice(-1) === '/'){
+    if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
         return hostPath.slice(0, -1)
-        
+
     }
-    
+
     return hostPath
 }
 
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
